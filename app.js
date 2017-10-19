e3a71c1fcd74625cb949b5411687ef1c90927174
nazmul56 <nazmul_104396@yahoo.com>
2017-10-19 13:12:52 +0600

    app.use() and Router

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function(req, res){
-   res.send("Hello World!");
-});
+var things = require('./things.js');
 
-app.post('/hello', function(req, res){
-   res.send("You just called the post method at '/hello'!\n");
-});
-app.all('/test', function(req, res){
-   res.send("HTTP method doesn't have any effect on this route!");
-});
+//both index.js and things.js should be in same directory
+app.use('/things', things);
+
 app.listen(3000);
