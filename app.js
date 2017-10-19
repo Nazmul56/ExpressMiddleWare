251b294b5a34bf716503936bcc0667109b3bed0d
nazmul56 <nazmul_104396@yahoo.com>
2017-10-19 13:28:39 +0600

    app.get() get id form route

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-var things = require('./things.js');
-
-//both index.js and things.js should be in same directory
-app.use('/things', things);
-
+app.get('/things/:name/:id', function(req, res) {
+   res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+});
 app.listen(3000);
