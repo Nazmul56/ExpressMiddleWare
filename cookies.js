e1036c802ee51a8de3c28e06dba3b1a2cd0b60d6
nazmul56 <nazmul_104396@yahoo.com>
2017-10-22 12:11:51 +0600

    Upload Cookies Example

---
+++ cookies.js
@@ -0,0 +1,16 @@
+var cookieParser = require('cookie-parser');
+var express = require('express');
+var app = express();
+
+app.use(cookieParser());
+
+app.get('/', function(req, res){
+   res.cookie('name', 'express').send('cookie set:  ' +  req.cookies.name); //Sets name = express
+  
+   console.log('Cookies: ' , req.cookies);
+});
+app.get('/clear_cookie_foo', function(req, res){
+   res.clearCookie('foo');
+   res.send('cookie foo cleared');
+});
+app.listen(3000);
